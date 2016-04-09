a7dceff32d9073fa91fe17e75000e7ec6cc19f81
Julia Connelly <juliakconnelly@gmail.com>
2016-04-08 22:26:22 -0500

    the into poot

--- background.js
+++ background.js
@@ -7,6 +7,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   console.log('Turning ' + tab.url + ' red!');
   chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
+    file: 'content.js'
   });
 });
