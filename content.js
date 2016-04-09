65905b889920803cde92aecfaeccd09d0f25cdd0
Kiya Govek <govekk@carleton.edu>
2016-04-08 23:23:23 -0500

    https images

--- content.js
+++ content.js
@@ -1,7 +1,13 @@
+/*
+* Carlhacks project by Joe Adkisson, Jon Bisila, Julia Connelly, and Kiya Govek
+*/
+
 var images = document.getElementsByTagName('img');
 
 for (var i = 0; i < images.length; i++) {
     var image = images[i];
     image.setAttribute('src','https://openmerchantaccount.com/img2/unicorn.png');
     image.setAttribute('srcset','https://openmerchantaccount.com/img2/unicorn.png');
+    //https://openmerchantaccount.com/img2/unicorn_tongue.png
+    //https://openmerchantaccount.com/img2/unicorn_cat.jpg
 }
