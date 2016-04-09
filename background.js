52b4a344e324580dca2d22b41f0ddd588317f2a0
Kiya Govek <govekk@carleton.edu>
2016-04-08 22:42:52 -0500

    removed console log

--- background.js
+++ background.js
@@ -5,7 +5,6 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
   chrome.tabs.executeScript({
     file: 'content.js'
   });
