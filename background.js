73c47c87c219520da0369d9c1a0d0c784e5ae9ec
Joe Adkisson <adkissonj@carleton.edu>
2016-04-09 01:38:15 -0500

    stuff

--- background.js
+++ background.js
@@ -2,6 +2,7 @@
 * Carlhacks project by Joe Adkisson, Jon Bisila, Julia Connelly, and Kiya Govek
 * Thanks to The Chromium Authors for ideas on source code formatting
 */
+
 var toggle = 0;
 
 // Called when a new page finishes updating
@@ -12,13 +13,19 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
     		file: 'content.js'
   		});
   }
- })
+})
 
-//chrome.addEventListener("load", function(tab) );
+// chrome.addEventListener("load", function(tab) {
+// 	if (toggle == 1) {
+//     	// do your things
+//     	chrome.tabs.executeScript({
+//     		file: 'content.js'
+//   		});
+//  	}
+//  });
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-	alert("squid");
 	if (toggle == 0) {
         chrome.browserAction.setIcon({path: "icon.png"});
   		toggle = 1;
