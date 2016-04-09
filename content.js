38189b01f4c2772bd83076103c14d8ad0e2d487b
Kiya Govek <govekk@carleton.edu>
2016-04-08 23:30:09 -0500

    Trying to merge content.js

--- content.js
+++ content.js
@@ -8,6 +8,8 @@ for (var i = 0; i < images.length; i++) {
     var image = images[i];
     image.setAttribute('src','https://openmerchantaccount.com/img2/unicorn.png');
     image.setAttribute('srcset','https://openmerchantaccount.com/img2/unicorn.png');
+    //https://openmerchantaccount.com/img2/unicorn_tongue.png
+    //https://openmerchantaccount.com/img2/unicorn_cat.jpg
 
     //image.setAttribute('src',makeImage());
 
