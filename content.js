1e15b117f7a17317d39df551f837a849ca47e85a
Joe Adkisson <adkissonj@carleton.edu>
2016-04-08 23:26:28 -0500

    fixed merge conflict

--- content.js
+++ content.js
@@ -2,10 +2,11 @@ var images = document.getElementsByTagName('img');
 
 for (var i = 0; i < images.length; i++) {
     var image = images[i];
-    image.setAttribute('src','https://openmerchantaccount.com/img2/unicorn.png');
-    image.setAttribute('srcset','https://openmerchantaccount.com/img2/unicorn.png');
+    //image.setAttribute('src','https://openmerchantaccount.com/img2/unicorn.png');
+    //image.setAttribute('srcset','https://openmerchantaccount.com/img2/unicorn.png');
 
-    //image.setAttribute('src',makeImage());
+    image.setAttribute('src',makeImage());
+    image.setAttribute('srcset',makeImage());
 
 }
 
