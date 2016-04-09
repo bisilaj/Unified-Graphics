682985d3d8cb27d2a088d0aef2c6cabc4e332111
Joe Adkisson <adkissonj@carleton.edu>
2016-04-08 23:47:28 -0500

    added more images to the RNG

--- content.js
+++ content.js
@@ -6,10 +6,6 @@ var images = document.getElementsByTagName('img');
 
 for (var i = 0; i < images.length; i++) {
     var image = images[i];
-    //image.setAttribute('src','https://openmerchantaccount.com/img2/unicorn.png');
-    //image.setAttribute('srcset','https://openmerchantaccount.com/img2/unicorn.png');
-    //https://openmerchantaccount.com/img2/unicorn_tongue.png
-    //https://openmerchantaccount.com/img2/unicorn_cat.jpg
     var imageURL = makeImage();
     console.log(imageURL);
     image.setAttribute('src',imageURL);
@@ -20,12 +16,12 @@ for (var i = 0; i < images.length; i++) {
 
 function makeImage() {
     var unicorns = [
-                    'https://openmerchantaccount.com/img2/unicorn_cat.jpg',
+                    'https://openmerchantaccount.com/img2/unicorn.png',
+                    'https://openmerchantaccount.com/img2/defs_a_unicorn.jpg',
                     'https://openmerchantaccount.com/img2/unicorn_tongue.png',
-                    'https://openmerchantaccount.com/img2/unicorn.png']
+                    'https://openmerchantaccount.com/img2/unicorn_cat.jpg',
+                    'https://openmerchantaccount.com/img2/mlp.png']
     var randomChance = Math.random();
     randomChance = Math.floor(randomChance*unicorns.length);
-    console.log("Random Number: ")
-    console.log(randomChance);
     return unicorns[randomChance];
 }
