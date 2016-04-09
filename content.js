b1055f9285b6819caa45751f49e8e9bbf2d60b97
Kiya Govek <govekk@carleton.edu>
2016-04-08 23:14:29 -0500

    hacked google

--- content.js
+++ content.js
@@ -2,5 +2,6 @@ var images = document.getElementsByTagName('img');
 
 for (var i = 0; i < images.length; i++) {
     var image = images[i];
-    image.setAttribute('src','http://img05.deviantart.net/28eb/i/2011/180/c/9/unicorn_poop_by_gremlinlegions-d3kh3s4.jpg');
+    image.setAttribute('src','https://openmerchantaccount.com/img2/unicorn.png');
+    image.setAttribute('srcset','https://openmerchantaccount.com/img2/unicorn.png');
 }
