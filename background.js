1ddcc7e4401c3e4f47f81a9bf8920971c8391924
Joe Adkisson <adkissonj@carleton.edu>
2016-04-09 00:12:26 -0500

    added toggle

--- background.js
+++ background.js
@@ -2,10 +2,23 @@
 * Carlhacks project by Joe Adkisson, Jon Bisila, Julia Connelly, and Kiya Govek
 * Thanks to The Chromium Authors for ideas on source code formatting
 */
+var toggle = 0;
+
+
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-    file: 'content.js'
-  });
+	if (toggle == 0) {
+  		toggle = 1;
+  		chrome.tabs.executeScript({
+    		file: 'content.js'
+  		});
+  	}
+  	else {
+  		toggle = 0;
+  		//off
+
+  	}
+
+
 });
