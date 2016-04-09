1631feea828a711a63f4ddff68dc3c523852b6d0
Kiya Govek <govekk@carleton.edu>
2016-04-09 15:56:14 -0500

    carlhacks_unicorn class

--- my-content.js
+++
@@ -1,6 +0,0 @@
-var images = document.getElementsByTagName('img');
-
-for (var i = 0; i < images.length; i++) {
-    var image = images[i];
-    image.setAttribute('src','http://img05.deviantart.net/28eb/i/2011/180/c/9/unicorn_poop_by_gremlinlegions-d3kh3s4.jpg');
-}
