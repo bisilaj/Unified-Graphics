98ada33a685414c8ea44b90a298ac90f1abcd84d
Joe Adkisson <adkissonj@carleton.edu>
2016-04-09 20:39:36 -0500

    tentative image selection algorithm. I think we'll need more images to see how well it ends up working

--- content.js
+++ content.js
@@ -30,9 +30,7 @@ function makeUnicorns(toggleState) {
 
     
 
-    console.log("Making some unicorns...");
     if(toggleState == true) {
-        console.log("Received go-ahead for unification...");
         replaceImgTags();
         replaceStyleImages();
     }
@@ -123,49 +121,77 @@ function makeUnicorns(toggleState) {
             return ""
         }
 
-var unicornList = [
-                    ['https://openmerchantaccount.com/img2/unicorn.png',1.1],
-                    ['https://openmerchantaccount.com/img2/defs_a_unicorn.jpg',1.1],
-                    ['https://openmerchantaccount.com/img2/unicorn_tongue.png',1.1],
-                    ['https://openmerchantaccount.com/img2/unicorn_cat.jpg',1.1],
-                    ['https://openmerchantaccount.com/img2/mlp.png',1.1]]
+
+
 
 
         function chooseImage(width, height) {
             if (!width || !height) {
+                console.log("values not found; using a random image.");
                 return randomImage();
             } else {
-                var ratio = width / height;
-                if (ratio == 1.0) {
-                    return randomSquareImage();
-                } else if (ratio > 1.3) {
-                    return 'https://openmerchantaccount.com/img2/unicorn.png';
-                } else if (ratio > 0.9) {
-                    return 'https://openmerchantaccount.com/img2/unicorn_tongue.png';
-                } else {
-                    return 'https://openmerchantaccount.com/img2/mlp.png';
-                }
+                return chooseBestImage(width, height);
+                // var ratio = width / height;
+                // if (ratio == 1.0) {
+                //     return randomSquareImage();
+                // } else if (ratio > 1.3) {
+                //     return 'https://openmerchantaccount.com/img2/unicorn.png';
+                // } else if (ratio > 0.9) {
+                //     return 'https://openmerchantaccount.com/img2/unicorn_tongue.png';
+                // } else {
+                //     return 'https://openmerchantaccount.com/img2/mlp.png';
+                // }
             }
         }
 
-        // // Chooses the image whose aspect ratio is the best fit for the image
-        // // that it's replacing. The 'ratio' argument, for the record, is 
-        // // width / height.
-        // function chooseBestImage(width, height) {
-        //     var ratio = width / height; // ratio of our image
-        //     if (ratio = 1) { return randomSquareImage()}
-        //     var bestImage = '';
-        //     var bestRatio = 0;
-        //     for (var i = 0; i < unicornList.length; i++) {
-        //         if(unicornList[i][1] = bestRatio) {
-        //         
-        //         }
-        //         if(unicornList[i][1] > bestRatio &&& unicornList[i][1] <= ratio) {
+        // Chooses the image whose aspect ratio is the best fit for the image
+        // that it's replacing. The 'ratio' argument, for the record, is 
+        // width / height.
+        function chooseBestImage(width, height) {
 
-        //         }
+            // url, width, height
+            var unicornList = [
+                    ['https://openmerchantaccount.com/img2/unicorn.png',264, 191],
+                    ['https://openmerchantaccount.com/img2/defs_a_unicorn.jpg',236, 211],
+                    ['https://openmerchantaccount.com/img2/unicorn_tongue.png',540,530],
+                    ['https://openmerchantaccount.com/img2/unicorn_cat.jpg',1920,1080],
+                    ['https://openmerchantaccount.com/img2/mlp.png',1024,1103]
+                  ];
 
-        //     }
-        // }
+
+            var ratio = width / height; // ratio of our image
+            // Square
+            if (ratio == 1.0) { return randomSquareImage(); }
+
+            // Not Square
+            var bestIndex = 0;
+            var bestRatio = 0;
+            var curRatio = 0;
+            for (var i = 0; i < unicornList.length-1; i++) {
+                // Find the ratio of the current entry
+                curRatio = unicornList[i][1]/unicornList[i][2];
+                // If they are the same Aspect Ratio
+                if(curRatio == bestRatio) {
+                    // If the new one is closer-sized to the original
+                    if(Math.abs(unicornList[i][1] - width) > Math.abs(unicornList[bestIndex][1] - width)) {
+                        bestIndex = i;
+                        // don't need to change bestRatio; they're the same.
+                    }
+                    // otherwise it stays the same.
+                }
+
+                // If the new one is better than current best
+                //if(curRatio > bestRatio && curRatio <= ratio) {
+                if(Math.abs(ratio - curRatio) < Math.abs(ratio - bestRatio)) {
+                    bestIndex = i;
+                    bestRatio = curRatio
+                }
+                // If the new one is worse, do nothing
+
+            }
+            console.log("best contender is image " + bestIndex + " with aspect ratio " + bestRatio);
+            return unicornList[bestIndex][0];
+        }
 
         function randomImage() {
             var unicorns = [
